68e61857141fe9b1cd279d60285dcde3d8c02a39
Matu36 <matipineda85@live.com.ar>
2024-04-25 19:02:04 -0300

    desc

--- api/src/controllers/Productos.js
+++ api/src/controllers/Productos.js
@@ -11,7 +11,7 @@ const getProductos = async (req, res) => {
             ({
               id,
               nombre,
-              descripcion,
+              // descripcion,
               marca,
               imagen,
               imagen1,
@@ -24,7 +24,7 @@ const getProductos = async (req, res) => {
             }) => ({
               id,
               nombre,
-              descripcion,
+              // descripcion,
               marca,
               imagen,
               imagen1,
@@ -58,7 +58,7 @@ const getProductoById = async (req, res) => {
       id: producto.id,
       nombre: producto.nombre,
       marca: producto.marca,
-      descripcion: producto.descripcion,
+      // descripcion: producto.descripcion,
       imagen: producto.imagen,
       imagen1: producto.imagen1,
       imagen2: producto.imagen2,
